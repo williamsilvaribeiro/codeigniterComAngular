ec977b2fb40d711c9a6b9c43ffe63b129ff7c7fd
William Ribeiro <williamsilva.ribeiro@gmail.com>
2015-11-30 18:26:42 -0200

    ajuste no layout

---
+++ application/helpers/csssistema_helper.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en" ng-app="app">
+<head>
+    <meta charset="UTF-8">
+    <title>CodeIgniter</title>
+    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+
+    <!-- jQuery -->
+    <!--  <script type="text/javascript" src="codAng/public/lib/jquery/jquery.min.js"></script>-->
+    <script src="<?php echo base_url();?>public/lib/jquery/jquery.min.js"></script>
+
+    <!-- Kendo -->
+    <script type="text/javascript" src="<?php echo base_url();?>public/lib/kendo-ui/js/kendo.web.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/lib/kendo-ui/styles/kendo.common-bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/lib/kendo-ui/styles/kendo.bootstrap.min.css"/>
+
+    <!-- Angular -->
+    <script type="text/javascript" src="<?php echo base_url();?>public/lib/angular/angular.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>public/js/controllers/pessoasCtrl.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>public/lib/angular-kendo-ui/build/angular-kendo.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/lib/bootstrap-css/css/bootstrap.min.css"/>
+
+    <!-- Messenger -->
+    <script type="text/javascript" src="<?php echo base_url();?>public/lib/messenger/build/js/messenger.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>public/lib/messenger/build/js/messenger-theme-flat.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/lib/messenger/build/css/messenger.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/lib/messenger/build/css/messenger-theme-air.css"/>
+
+
+
+</head>
